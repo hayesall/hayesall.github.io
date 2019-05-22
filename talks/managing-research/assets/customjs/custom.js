395bd1006d8a218502209cd95119a5b38842e83c
batflyer <alexander@batflyer.net>
2019-05-22 09:08:46 -0400

    Moving js to common location to fix build error

--- talks/managing-research/assets/customjs/custom.js
+++ talks/managing-research/assets/customjs/custom.js
@@ -1,3 +1,3 @@
 // Push the reveald3 dependency to options.dependencies
 
-options.dependencies.push({src: './assets/node_modules/reveald3/reveald3.js', async: true})
+options.dependencies.push({src: './assets/customjs/reveald3.js', async: true})
